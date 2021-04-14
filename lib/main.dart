80c57f309a412b0e8f69db7aa962d4a43a85ba48
Faris Čolaković <fcolakovic2@etf.unsa.ba>
2021-04-14 13:24:40 +0200

    zavrsen dizajn kartice, potrebno izvuci varijable

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:googleservices/screens/main_screen.dart';
+import 'package:googleservices/widgets/kartica.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainScreen(),
+      home: KarticaWidget(),
     );
   }
 }
--- lib/widgets/kartica.dart
+++ lib/widgets/kartica.dart
@@ -1,8 +1,46 @@
 import 'package:flutter/material.dart';
 
-class kartica extends StatelessWidget {
+class KarticaWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Column(children: [
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              width: 335,
+              height: 174,
+              child: Card(
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: 335,
+                      height: 110,
+                      child: Image.asset(
+                        'assets/images/test.jpg',
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    Expanded(
+                        child: Column(children: [
+                      Text('Firebase'),
+                      Text('GoogleServices')
+                    ]))
+                  ],
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                elevation: 5,
+                margin: EdgeInsets.all(10),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
